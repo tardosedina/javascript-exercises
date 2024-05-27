4ff2afb9151ed867f1702a4582fda8d8d54eeff5
tardosedina <102327051+tardosedina@users.noreply.github.com>
2024-05-27 02:09:29 +0200

    Fix code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str="";
+    for(let i=1; i<=num; i++) {
+        str += string;
+    }
+    return str;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
