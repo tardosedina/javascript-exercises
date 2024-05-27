73bdd81a163a890222ca311abce65aa142768995
tardosedina <102327051+tardosedina@users.noreply.github.com>
2024-05-28 01:59:00 +0200

    Write sum code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(fNum, sNum) {
+    let aNum = 0;
+    for(let i = fNum; i<=sNum; i++) {
+        aNum+=i;
+    }
+    return aNum;
 };
 
 // Do not edit below this line
