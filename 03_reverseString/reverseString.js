8f5a33238795a499d0e9a8fd4c083f0efbe8bd95
tardosedina <102327051+tardosedina@users.noreply.github.com>
2024-05-27 02:25:44 +0200

    Fix code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string = str.split('');
+    let vissza = string.reverse();
+    let vegso = vissza.join('');
+    return vegso;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
