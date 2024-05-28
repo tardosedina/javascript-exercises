1ff7236632ef239888392045d4a2c8ddabbd5404
tardosedina <102327051+tardosedina@users.noreply.github.com>
2024-05-28 02:39:38 +0200

    Write calc functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(aNm, bNm) {
+	return aNm + bNm;
 };
 
-const subtract = function() {
-	
+const subtract = function(aSn, bSn) {
+	return aSn - bSn;
 };
 
-const sum = function() {
-	
+const sum = function(s1n, s2n) {
+	let sum = s1n + s2n;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(m1n, m2n) {
+  return m1n * m2n;
 };
 
-const power = function() {
-	
+const power = function(p1n, p2n) {
+	return p1n%p2n;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 0;
+	while(num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
