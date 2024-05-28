b34f3bede464e5c8f3e83f9f756330d42c95082c
tardosedina <102327051+tardosedina@users.noreply.github.com>
2024-05-28 02:30:52 +0200

    Add conversion logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celsius) {
+  let fahrenheit = Math.round((celsius - 32) / 1.8);
+  return fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheit) {
+  let celsius = Math.round((fahrenheit * 1.8) + 32);
+  return celsius;
 };
 
 // Do not edit below this line
